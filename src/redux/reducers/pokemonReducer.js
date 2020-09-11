02df897e8312c6453f79f51912cc8cc24f4d10c5
David Elì <davidelimg1@gmail.com>
2020-09-11 14:15:21 -0500

    Added initial state

--- src/redux/reducers/pokemonReducer.js
+++ src/redux/reducers/pokemonReducer.js
@@ -1,22 +1,4 @@
-const pokemonReducer = (state = [{
-  id: 1,
-  title: 'Securing React Apps with Auth0',
-  slug: 'react-auth0-authentication-security',
-  authorId: 1,
-  category: 'JavaScript',
-},
-{
-  id: 2,
-  title: 'React: The Big Picture',
-  slug: 'react-big-picture',
-  authorId: 1,
-  category: 'JavaScript',
-},
-{
-  id: 3,
-  title: 'Creating Reusable React Components',
-  slug: 'react-creating-reusable-components',
-  authorId: 1,
-  category: 'JavaScript',
-}]) => state;
+import initialState from './initialState';
+
+const pokemonReducer = (state = initialState.pokemons) => state;
 export default pokemonReducer;
