cc596da1a25f3706df764218bfe6ce745caf8b01
David Elì <davidelimg1@gmail.com>
2020-09-13 19:38:23 -0500

    Added boxes to pokemons page

--- src/components/pokemons/PokemonsList.js
+++ src/components/pokemons/PokemonsList.js
@@ -6,31 +6,36 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import capitalizeWord from '../../tools/capitalizeWord';
 
+const styles = {
+  li: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    listStyleType: 'none',
+    borderBottom: '5px solid #3F51B5',
+    borderLeft: '5px solid #3F51B5',
+    borderRight: '5px solid #3F51B5',
+  },
+  name: {
+    borderLeft: '5px solid #3F51B5',
+    borderRight: '5px solid #3F51B5',
+    borderBottom: '5px solid #3F51B5',
+    width: '70%',
+    textAlign: 'center',
+  },
+};
+
 const PokemonList = ({ pokemons }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th />
-        <th>Name</th>
-        <th>Height</th>
-        <th>Weight</th>
-      </tr>
-    </thead>
-    <tbody>
-      {pokemons.map(pokemon => (
-        <tr key={pokemon.id}>
-          <td>
-            <img src={pokemon.sprites.other['official-artwork'].front_default} height="100px" width="100px" />
-          </td>
-          <td>
-            <Link to={`/pokemons/${pokemon.name}`}>{capitalizeWord(pokemon.name)}</Link>
-          </td>
-          <td>{pokemon.height}</td>
-          <td>{pokemon.weight}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+  <ul>
+    {pokemons.map(pokemon => (
+      // eslint-disable-next-line max-len
+      <li key={pokemon.id} style={styles.li}>
+        <Link to={`/pokemons/${pokemon.name}`} style={styles.name}><h2>{capitalizeWord(pokemon.name)}</h2></Link>
+        <img src={pokemon.sprites.other['official-artwork'].front_default} height="250px" width="250px" />
+      </li>
+    ))}
+  </ul>
 );
 
 PokemonList.propTypes = {
