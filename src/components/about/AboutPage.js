1b8944065cc632746f79cea562bb534896db7433
David Elì <davidelimg1@gmail.com>
2020-09-14 21:52:15 -0500

    Styling finished

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+const spanStyle = {
+  fontSize: '15px',
+  color: '#757575',
+};
+
 const AboutPage = () => (
   <div className="jumbotron">
     <h1>About</h1>
     <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
+      An application that displays information about pokemons.
+    </p>
+    <p>
+      <span style={spanStyle}>
+        Created by
+        <a href="https://www.linkedin.com/in/davideligarcia/" target="_blank" rel="noreferrer"> David García </a>
+        Github:
+        <a href="https://github.com/davitomix" target="_blank" rel="noreferrer"> @davitomix</a>
+      </span>
     </p>
   </div>
 );
